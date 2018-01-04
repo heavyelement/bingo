c5fa3d7b405ebba20621637a34ad9e8a7610debc
Gardiner <heavyelement>
2018-01-04 12:53:50 -0500

    Moved the free space prediction to the predictions.json file. Updated the loop to get the free space code from there.

--- index.php
+++ index.php
@@ -1,42 +1,52 @@
 <link rel='stylesheet' type='text/css' href='/bingo/bingo.css'>
 
-    <h1>2018 Tech News Bingo</h1>
-    <form autocomplete="off">
-<table>
-    <!-- <tr>
-        <th>One</th>
-        <th>Two</th>
-        <th>Thee</th>
-        <th>Four</th>
-        <th>Five</th>
-    </tr> -->
-<?php
+<h1>2018 Tech News Bingo</h1>
+<form autocomplete="off">
+    <table>
+        <!-- <tr>
+            <th>One</th>
+            <th>Two</th>
+            <th>Thee</th>
+            <th>Four</th>
+            <th>Five</th>
+        </tr> -->
+    <?php
 
-    $predictions = json_decode(file_get_contents("bingo/predictions.json"),true);
-    $i=0;
-    foreach($predictions as $key => $value){
+        // Get and decode the predictions file.
+        $predictions = json_decode(file_get_contents("bingo/predictions.json"),true);
+        $i=0;
+
+        foreach($predictions as $key => $value){
             if($i % 5 === 0){
+                // Create table rows
                 echo "<tr>";
             }
             if($i === 12){
+                // Generate free space
                 echo "<td id='cell$i'>";
                 echo "<input type='checkbox' id='freeSpace' checked disabled></input>";
                 echo "<label for='label$i' class='background'></label>";
-                echo "<p class='text'>Steam continues getting worse<br>(Free Space)</p>";
-                echo "<div id='punchline$i' class='punchlines'><div>Steam Direct continues to pollute the storefront with filth and bile. Valve keeps making money and don't care at all.<div></div>";
+                echo "<p class='text'>" . $predictions["freespace"]["setup"] . "<br>(Free Space)</p>";
+                echo "<div id='punchline$i' class='punchlines'><div>" . $predictions["freespace"]["punchline"] . "<div></div>";
                 echo "</td>";
+                // Update counter, this allows the current square to be
+                // generated while also adding a free space where it's supposed to be.
                 $i++;
             }
             echo "<td id='cell$i'>";
             echo "<input type='checkbox' id='label$i'";
             if(isset($value["selected"])){
+                // Make the current input checked if it has a 'selected' key
+                // Should it also be disabled to prevent it from being unchecked??
                 echo " checked";
             }
-<<<<<<< HEAD
-            echo "></input>";
-=======
+
+            // Added a call to a JS function to check for a winner.
+            // I should probably actually write that.
             echo " onchange='checkForWinner()'></input>";
->>>>>>> dbfda2890a43d3f6a9288bb1387d662b8a3ac79b
+
+            // Source order is important here. The <label> element needs to immediately follow the checkbox
+            // in order for the <label> styling to work as intended.
             echo "<label for='label$i' class='background'></label>";
             echo "<p class='text'>" . $value['setup'] . "</p>";
             echo "<div id='punchline$i' class='punchlines";
@@ -46,16 +56,21 @@
             echo "'><div>" . $value['punchline'] . "</div></div>";
             echo "</td>";
             if($i % 5 === 4){
+                
+                // Where appropriate, close the row
                 echo "</tr>";
             }
             if($i >= 24){
+                // End the bingo card when the counter gets to 24
                 break;
             }
+
+            // Finally, increment the counter
             $i++;
-    }
+        }
 
 
-?>
-</table>
+    ?>
+    </table>
 </form>
 <br><br><br><br><br><br><br>
